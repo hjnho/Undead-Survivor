99ba85f39a212653b4d2cfac2c4df045288daad5
hjnho <nhobk@naver.com>
2023-07-10 14:22:43 +0900

    소환 레벨 적용
    뱀서라이크 강의 6+

--- Assets/Undead Survivor/Script/Spawner.cs
+++ Assets/Undead Survivor/Script/Spawner.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
+
+    int level;
 
     float timer;
 
@@ -16,8 +19,9 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f),spawnData.Length-1);
 
-        if(timer > 0.2f)
+        if(timer > spawnData[level].spawnTime)
         {
             timer = 0f;
             Spawn();            
@@ -26,7 +30,18 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,2));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public float spawnTime;
+    public int spriteType;
+   
+    public int health;
+    public float speed;
 }
